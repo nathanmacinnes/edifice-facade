d004b39c607a1d1d8213155b4c7a377fac2be596
Nathan MacInnes <nathan@macinn.es>
2014-04-20 09:52:11 -0400

    Initial functionality

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ edifice-facade
 ==============
 
 Create facades to separate modules in your application
+
+Facades rely on a core which can be as simple as an EventEmitter. They separate
+modules from each other, so that the modules communicate with each other using
+events.
+
+````
+var EventEmitter = require("events").EventEmitter,
+    Facade = require("edifice-facade");
+
+var core = new EventEmitter();
+
+var fileInterfaceFacade = new Facade(core,
+    ['fileDataRead', 'fileWritten'], // permitted actions
+    ['newInputData'] // permitted listeners
+);
+var userInterfaceFacade = new Facade(core,
+    ['newInputData'],
+    ['fileDataRead', 'fileWritten']
+);
+````
+
+Now pass these facades to your `fileInterface` and `userInterface`, so they
+don't need to have each other or the core itself in scope.
---
+++ test/testFacade.js
@@ -0,0 +1,80 @@
+var expect = require("expect.js"),
+    injectr = require("injectr"),
+    pretendr = require("pretendr");
+
+describe("Facade", function () {
+    var Facade,
+        mockCore;
+    it("should inherit from EventEmitter", function () {
+        var requires;
+        requires = pretendr({
+            util : {
+                inherits : function () {}
+            },
+            events : {
+                EventEmitter : function () {}
+            }
+        });
+        Facade = injectr("../lib/facade.js", requires.mock).Facade;
+        expect(requires.util.inherits.calls).to.have.length(1);
+        expect(requires.util.inherits.calls[0].args)
+            .to.have.property(0, Facade)
+            .and.to.have.property(1, requires.events.EventEmitter.mock);
+    });
+    it("makes the array properties optional", function () {
+        Facade = injectr("../lib/facade.js").Facade;
+        expect(new Facade({})).to.be.a(Facade);
+    });
+
+    describe("#emit", function () {
+        beforeEach(function () {
+            Facade = injectr("../lib/facade.js").Facade;
+            mockCore = pretendr({
+                on : function () {},
+                emit : function () {}
+            });
+        });
+        it("passes emits to the core for listed actions", function () {
+            var eventData = {},
+                facade = new Facade(mockCore.mock, ['a'], []);
+            facade.emit('a', eventData);
+            expect(mockCore.emit.calls).to.have.length(1);
+            expect(mockCore.emit.calls[0].args)
+                .to.have.property(0, 'a')
+                .and.to.have.property(1, eventData);
+        });
+        it("doesn't pass emits of non-listed actions", function () {
+            var facade = new Facade(mockCore.mock, ['a'], []);
+            facade.emit('c', {});
+            expect(mockCore.emit.calls).to.have.length(0);
+        });
+    });
+    
+    describe("#on", function () {
+        beforeEach(function () {
+            Facade = injectr("../lib/facade.js").Facade;
+            mockCore = pretendr({
+                on : function () {},
+                emit : function () {}
+            });
+        });
+        it("passes handlers to the core for listed listeners", function () {
+            var fn = pretendr(function () {}),
+                facade = new Facade(mockCore.mock, [], ['a']);
+            facade.on('a', fn.mock);
+            expect(mockCore.on.calls).to.have.length(1);
+            expect(mockCore.on.calls[0].args)
+                .to.have.property(0, 'a');
+            mockCore.on.calls[0].args[1]('foo');
+            expect(fn.calls).to.have.length(1);
+            expect(fn.calls[0].args).to.have.property(0, 'foo');
+        });
+        it("doesn't pass handlers for non-listed listeners", function () {
+            var facade = new Facade(mockCore.mock, [], ['a', 'b']);
+            facade.on('c', function () {});
+            expect(mockCore.on.calls).to.have.length(2);
+            expect(mockCore.on.calls[0].args).to.have.property(0, 'a');
+            expect(mockCore.on.calls[1].args).to.have.property(0, 'b');
+        });
+    });
+});
